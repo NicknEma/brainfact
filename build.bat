93c757edc5201e94536caf88779788feb2bfc62b
NicknEma <62065135+NicknEma@users.noreply.github.com>
2024-02-29 17:11:43 +0100

    Make build.bat produce a map file

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ rc /nologo res/resources.rc
 
 if not exist debug mkdir debug
 del debug/bf.pdb > NUL 2> NUL
-odin build src -debug -o:none -out:debug/bf.exe -vet -warnings-as-errors -extra-linker-flags:res/resources.res
+odin build src -debug -o:none -out:debug/bf.exe -vet -warnings-as-errors -extra-linker-flags:"res/resources.res /map:debug/bf.map"
 del res/resources.res > NUL 2> NUL
